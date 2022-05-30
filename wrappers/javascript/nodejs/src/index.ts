42b0f0911927bdc3930330d1b751c3c36b19e1ce
Berend Sliedrecht <berend@animo.id>
2022-05-30 10:16:51 +0200

    feat(nodejs): version binding and cleanup
    Signed-off-by: Berend Sliedrecht <berend@animo.id>

--- wrappers/javascript/nodejs/src/index.ts
+++ wrappers/javascript/nodejs/src/index.ts
@@ -1,6 +1,11 @@
 import { nativeIndyCredx } from './lib'
+import { registerIndyCredx } from 'indy-credx-shared'
 
 const run = () => {
+  // NOTE: this is only required when using the functionality from within shared
+  // if you want to access the library like in the `console.log` below, you can
+  // do that directly
+  registerIndyCredx({ credx: nativeIndyCredx })
   console.log(nativeIndyCredx.credx_version())
 }
 
---
+++ wrappers/javascript/shared/src/register.ts
@@ -0,0 +1,4 @@
+// TODO: types
+export let indyCredx: any
+
+export const registerIndyCredx = ({ credx: askar }: { credx: any }) => (indyCredx = askar)
